1e98f8252c07d040768c73debd753932bba18c0c
Moein Siami <moeins1216@gmail.com>
2024-09-10 14:48:13 -0700

    admin panel customize

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
+    ordering = ['title', 'publish']
+    list_filter = ['status', 'author', 'publish']
+    search_fields = ['title', 'description']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'slug': ['title']}
+    list_editable = ['status']
+    # list_display_links = ['author']
 
