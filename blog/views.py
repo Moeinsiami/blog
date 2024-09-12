a30d044681d8991eda930fb598f50c8e121d0206
Moein Siami <moeins1216@gmail.com>
2024-09-12 10:06:39 -0700

    define post_list and post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.http import Http404
 
 
 def index(request):
@@ -6,8 +8,19 @@ def index(request):
 
 
 def post_list(request):
-    pass
+    posts = Post.published.all()
+    context = {
+        'posts': posts,
+    }
+    render(request, '', context)
 
 
-def post_detail(request):
-    pass
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except:
+        raise Http404('Post does not exist')
+    context = {
+        'post': post,
+    }
+    render(request, '', context)
