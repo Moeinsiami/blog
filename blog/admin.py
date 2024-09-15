b64d4c6bcfb21a6129f616f5018b3ec6502472f4
Moein Siami <moeins1216@gmail.com>
2024-09-15 14:59:32 +0330

    jalali date manipulate

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from .models import *
+from django_jalali.admin.filters import JDateFieldListFilter
+import django_jalali.admin as jadmin
 
 
-admin.sites.AdminSite.site_header = "پنل مدیر جنگو"
-admin.sites.AdminSite.site_title = "پنل مدیر"
-admin.sites.AdminSite.index_title = "پنل"
+# admin.sites.AdminSite.site_header = "پنل مدیر جنگو"
+# admin.sites.AdminSite.site_title = "پنل مدیر"
+# admin.sites.AdminSite.index_title = "پنل"
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publish', 'status']
     ordering = ['title', 'publish']
-    list_filter = ['status', 'author', 'publish']
+    list_filter = ['status', 'author', ('publish', JDateFieldListFilter)]
     search_fields = ['title', 'description']
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
--- sabzweb/settings.py
+++ sabzweb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'django_jalali',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'fa-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
