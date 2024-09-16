a97f949ceab9b1104c57add9bc63c2da13545239
Moein Siami <moeins1216@gmail.com>
2024-09-16 23:46:49 +0330

    define ticket function

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 
-from blog.models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from blog.models import *
+from blog.forms import *
+
 
 def index(request):
     pass
@@ -40,3 +41,21 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
+
+
+def ticket(request):
+    ticket_obj = Ticket.objects.create()
+    if request.method == "POST":
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            ticket_obj.message = cd['message']
+            ticket_obj.name = cd['name']
+            ticket_obj.email = cd['email']
+            ticket_obj.phone = cd['phone']
+            ticket_obj.subject = cd['subject']
+            ticket_obj.save()
+            redirect('blog:index')
+    else:
+        form = TicketForm()
+    return render(request, 'forms/ticket.html',{'form':form})
