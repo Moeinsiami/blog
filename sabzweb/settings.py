a5aa91d8205f5fb76109a716c23b7cb5d5f2c039
Moein Siami <moeins1216@gmail.com>
2024-09-29 21:05:01 +0330

    migrate to psql

--- sabzweb/settings.py
+++ sabzweb/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'sabzweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog_admin',
+        'PASSWORD': '13.84.Mmm',
+        'PORT': '5432',
     }
 }
 
