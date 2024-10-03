ddd1c3d37928c0f9bb4df548cb8eb0db5658d535
Moein Siami <moeins1216@gmail.com>
2024-10-03 22:09:12 +0330

    add post_search url

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('posts/<pk>', views.post_detail, name='post_detail'),
     path('posts/<post_id>/comment', views.post_comment, name='post_comment'),
     path('ticket/', views.ticket, name='ticket'),
+    path('search/', views.post_search, name='post_search'),
 
 ]
