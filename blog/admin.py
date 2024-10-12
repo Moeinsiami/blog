4eddf5795419a0d4c91dda71c19bd7ccd6e1e98c
Moein Siami <moeins1216@gmail.com>
2024-10-12 15:29:22 +0330

    add inline to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from pip._vendor.rich.markup import Tag
+
 from .models import *
 from django_jalali.admin.filters import JDateFieldListFilter
 import django_jalali.admin as jadmin
@@ -7,6 +9,16 @@ import django_jalali.admin as jadmin
 # admin.sites.AdminSite.site_header = "پنل مدیر جنگو"
 # admin.sites.AdminSite.site_title = "پنل مدیر"
 # admin.sites.AdminSite.index_title = "پنل"
+
+#Inlines
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 0
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
 
 
 @admin.register(Post)
@@ -20,11 +32,13 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
     list_editable = ['status']
     # list_display_links = ['author']
+    inlines = [ImageInline, CommentInline]
 
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ['name', 'subject', 'phone']
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
@@ -32,3 +46,8 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ['active', ('created', JDateFieldListFilter), ('updated', JDateFieldListFilter)]
     search_fields = ['name', 'body']
     list_editable = ['active']
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['post', 'title', 'created']
