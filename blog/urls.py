3d6a6fb03a4c7cbd87a7d10ec7ab7fdc43275b08
Moein Siami <moeins1216@gmail.com>
2024-10-16 00:13:27 +0330

    add urls

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
     path('posts/<post_id>/comment', views.post_comment, name='post_comment'),
     path('ticket/', views.ticket, name='ticket'),
     path('search/', views.post_search, name='post_search'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/create_post', views.create_post, name='create_post'),
+    path('profile/create_post/<post_id>', views.edit_post, name='create_edit'),
+    path('profile/delete_post/<post_id>', views.delete_post, name='delete_post'),
+    path('profile/delete_image/<image_id>', views.delete_image, name='delete_image'),
 
 ]
