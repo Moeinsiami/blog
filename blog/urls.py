4f182eed4fc87489ad40500e1a020700ccd998ef
Moein Siami <moeins1216@gmail.com>
2024-10-18 19:53:16 +0330

    add login and logout urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'blog'
 
@@ -17,5 +18,7 @@ urlpatterns = [
     path('profile/create_post/<post_id>', views.edit_post, name='create_edit'),
     path('profile/delete_post/<post_id>', views.delete_post, name='delete_post'),
     path('profile/delete_image/<image_id>', views.delete_image, name='delete_image'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 
 ]
