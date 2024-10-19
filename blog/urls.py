ba034376c6765ebced0bec4a6cfc1687255b0d71
Moein Siami <moeins1216@gmail.com>
2024-10-20 02:41:55 +0330

    add post_list url

--- blog/urls.py
+++ blog/urls.py
@@ -6,10 +6,12 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts/', views.post_list, name="post_list"),
+    path('posts/<str:category>', views.post_list, name="post_list_category"),
     # path('posts/', views.post_list, name='post_list'),
-    path('posts/', views.PostListView.as_view(), name='post_list'),
+    # path('posts/', views.PostListView.as_view(), name='post_list'),
     # path('posts/<int:id>', views.post_detail, name='post_detail'),
-    path('posts/<pk>', views.post_detail, name='post_detail'),
+    path('posts/detail/<pk>', views.post_detail, name='post_detail'),
     path('posts/<post_id>/comment', views.post_comment, name='post_comment'),
     path('ticket/', views.ticket, name='ticket'),
     path('search/', views.post_search, name='post_search'),
