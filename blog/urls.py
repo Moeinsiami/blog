436bb6def9d2ee3ddb0454b840f293296942fe13
Moein Siami <moeins1216@gmail.com>
2024-10-20 00:02:39 +0330

    add register url

--- blog/urls.py
+++ blog/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url='/blog/password-reset/complete'), name="password_reset_confirm"),
     path('password-reset/complete/',auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete.html"),
-
+    path('register/', views.register, name="register"),
 ]
